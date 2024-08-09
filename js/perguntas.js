f54a631513fcd9e8b8846633c2de3c1f6fa39db8
Antonio <107552515+antonioo23@users.noreply.github.com>
2024-08-09 08:53:51 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,110 @@
+export const perguntas = [
+    {
+        enunciado: "O que você pensa sobre a inteligência artificial?",
+        alternativas: [
+
+            {
+                texto: "Bom.",
+                afirmacao: [
+                "Algo bom! É uma forma mais prática para acessar as informações.",
+                "Fico feliz, pois me auxilia em meu dia a dia!"
+                ]
+            },
+            {
+                texto: "Ruim.",
+                afirmacao: [
+                    "Não agrada, pois da um conhecimento supérfluo.",
+                    "Não é usada de maneira correta por muitas pessoas."
+           
+                ]     
+            },
+
+        ]
+    },
+
+    {
+        enunciado: "Ela pode ser usada para estudos?",
+        alternativas: [
+
+            {
+                texto: "Sim.",
+                afirmacao: [
+                    "Caso seja usada de maneira correta, pode ajudar muito.",
+                    "Seu auxílio é essencial."
+                ]
+            },
+            {
+                texto: "Não.",
+                afirmacao: [
+                    "Não é coerente sua utilização.",
+                    "Plagiar não é aprender."
+                ]
+            },
+
+        ]
+    },
+
+    {
+        enunciado: "Qual é uma das preocupações éticas relacionadas à IA?",
+        alternativas: [
+
+            {
+                texto: "Violação da privacidade e discriminação algorítmica.",
+                afirmacao: [
+                    "Existe uma grande preucupação de para onde nosso histórico de busca pode estar indo, assim invadindo e discriminalizando seus usuários.",
+                    "Grandes empresas, como, Facebook e Instagran compram seu algoritimo."
+                ]
+            },
+            {
+                texto: "Desenvolvimento de novas tecnologias.",
+                afirmacao: [
+                    "O desenvolvimento de novas tecnologias é algo benéfico para as pessoas, e não algo em que devemos nos preocupar.",
+                    "Elas nos auxíliam todos os dias."
+                ]
+            },
+
+        ]
+    },
+
+    {
+        enunciado: "Em qual área a Visão Computacional é amplamente utilizada?",
+        alternativas: [
+            {
+                texto: "Reconhecimento de voz.",
+                afirmacao: [
+                    "Por mais que existam as assistentes virtuais, elas não a área maia ampla onde a IA é utilizada.",
+                    "Há muitas áreas além dessa."
+                ]
+            },
+            {
+                texto: "Robótica.",
+                afirmacao: [
+                    "A área da robótica é mais amplamente usada, seja na criação de braços robóticos,",
+                    "Seu uso está mais diretamente ligado a robótica."
+                ]
+            },
+
+        ]
+    },
+
+    {
+        enunciado: "Qual é um dos maiores desafios éticos relacionados à Inteligência Artificial?",
+        alternativas: [
+            {
+                texto: "A falta de controle humano sobre as máquinas.",
+                afirmacao: [
+                    "Com o avanço das inteligências artificias, pode-se chegar a um ponto em que elas se auto comandem.",
+                    "E se elas criarem vida própria."
+                ]
+            },
+            {
+                texto: "A falta de aplicabilidade prática da IA.",
+                afirmacao: [
+                    "Pois na nossa atualidade ela está em praticamente todos os aspectos sociais.",
+                    "usamos ela todos os dias sem nos darmos conta."
+                ]
+            },
+
+        ]
+    }
+];
